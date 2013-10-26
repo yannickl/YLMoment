9313f80ba7fa3e8041b939a37353ca924c2500b5
Yannick Loriot <yannick.loriot@gmail.com>
2013-10-26 20:42:04 +0200

    [REFACTORING] Example + Test projects

---
+++ Tests/iOSTests/NSMomentTests+Format.m
@@ -0,0 +1,23 @@
+//
+//  NSMomentTests+Format.m
+//  NSMomentTests
+//
+//  Created by YannickL on 26/10/2013.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "Expecta.h"
+
+@interface NSMomentTests_Format : XCTestCase
+
+@end
+
+@implementation NSMomentTests_Format
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
