b42062f6ef36b1d866b212c1b1bd2da98647738f
Yannick Loriot <yannick.loriot@gmail.com>
2013-10-27 11:55:14 +0100

    [NEW] test case

--- Tests/iOSTests/NSMomentTests+Format.m
+++ Tests/iOSTests/NSMomentTests+Format.m
@@ -7,12 +7,22 @@
 //
 
 #import <XCTest/XCTest.h>
+
+#define EXP_SHORTHAND
 #import "Expecta.h"
+
+#import "NSMoment.h"
 
 @interface NSMomentTests_Format : XCTestCase
 
 @end
 
 @implementation NSMomentTests_Format
+
+- (void)testFomatWith_yy
+{
+    NSMoment *reference = [NSMoment momentWithDateAsString:@"2009/04/05"];
+    expect([reference format:@"yy"]).to.equal(@"09");
+}
 
 @end
